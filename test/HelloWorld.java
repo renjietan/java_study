284406b4cb11c6a6bf80adf3916abeebb4598798
renjietan <1694805460@qq.com>
2022-01-18 00:20:37 +0800

    1

---
+++ test/HelloWorld.java
@@ -0,0 +1,16 @@
+// public class HelloWorld {
+//     public static void main(String[] args) {
+//         System.out.println("Hello world");
+//     }
+// }
+
+class A {
+
+}
+class B {
+
+}
+
+public class HelloWorld {
+
+}
